a7945c3bd15cdbb0949d85f2a69a4079b03941f5
doooona <doooona.lee@gmail.com>
2014-04-10 23:11:30 +0800

    [POC4] add new fetcher

---
+++ PowerSave/src/com/apps/gogo/powersave/ThemeFetcher.java
@@ -0,0 +1,59 @@
+package com.apps.gogo.powersave;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Environment;
+import android.util.Log;
+
+public class ThemeFetcher {
+	
+	private List<File> themeFiles;
+	public List<File> getAllThemeFiles(){
+		
+		themeFiles = new ArrayList<File>(); 
+		String downloadFolderStr = Environment.getExternalStorageDirectory()+File.separator+"Download";
+		
+		File downloadFolder = new File(downloadFolderStr);
+		
+		if(downloadFolder !=null && downloadFolder.isDirectory() && downloadFolder.exists()){
+			
+			this.getNextSearch(downloadFolder);
+		}else{
+			
+			downloadFolderStr = Environment.getExternalStorageDirectory()+File.separator+"download";
+			
+			File downloadFolder2 = new File(downloadFolderStr);
+			
+			if(downloadFolder2 !=null && downloadFolder2.isDirectory() && downloadFolder2.exists()){
+				
+				this.getNextSearch(downloadFolder2);
+			}
+		}
+		
+		return themeFiles;
+	}
+	
+	private void getNextSearch(File downloadFolder){
+		
+		File[] subDictory = downloadFolder.listFiles();
+		
+		for(File subFile:subDictory){
+			if(subFile.isFile()){
+				
+				if(subFile.getName().indexOf("themefile") != -1){
+					themeFiles.add(subFile);
+					Log.d("@@@","find theme: "+ subFile.getName());
+				}
+			}else{
+				
+				if(subFile.isDirectory()){
+					this.getNextSearch(subFile);
+				}
+			}
+			
+		}
+		
+	}
+}
