cda8f0b0b4b4191bf49096b7406d18e9839b81dd
doooona <doooona.lee@gmail.com>
2014-05-19 07:33:23 +0800

    []POC10 2

--- Send2SD/src/com/easy/apps/send2sd/MainActivity.java
+++ Send2SD/src/com/easy/apps/send2sd/MainActivity.java
@@ -1,15 +1,22 @@
 package com.easy.apps.send2sd;
 
+import info.hoang8f.widget.FButton;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements View.OnClickListener{
+	
+	private FButton twitterBtn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		twitterBtn = (FButton) findViewById(R.id.f_twitter_button);
+		twitterBtn.setButtonColor(this.getResources().getColor(R.color.color_sun_flower));
 	}
 
 	@Override
@@ -17,6 +24,20 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		int id = v.getId();
+		
+		switch(id){
+		case R.id.f_twitter_button:
+			break;
+		
+		case R.id.settings_button:
+			break;
+		}
 	}
 
 }
